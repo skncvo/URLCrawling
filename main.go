d206e45cfaa1a420f2e9fd0f6d6fc14f8bbc7b54
skncvo <wm3233@naver.com>
2025-06-29 15:52:34 +0900

    getpage update (id, title, condition, job_sector)긁어오도록

--- main.go
+++ main.go
@@ -13,15 +13,44 @@ var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchwo
 
 func main() {
 	totalPages := getPages()
-	for i := 0; i < totalPages; i++ {
+	for i := 1; i <= totalPages; i++ {
 		getPage(i)
+		fmt.Println()
 	}
+
 }
 
 // page url 불러오기
 func getPage(page int) {
 	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(page)
 	fmt.Println("Request:", pageURL)
+
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	// https://www.saramin.co.kr/zf_user/jobs/relay/view?rec_idx=공고ID
+
+	doc.Find(".item_recruit").Each(func(i int, s *goquery.Selection) {
+		// Attr : attribute(속성), value, id, class, href ...etc
+		id, _ := s.Attr("value")
+
+		// job_tit의 a를 찾는다
+		title := s.Find(".job_tit>a").Text()
+		condition := s.Find(".job_condition").Text()
+
+		// 특정 span 적용 안되게
+		jobsel := s.Find(".job_sector").Clone()
+		jobsel.Find(".job_day").Remove()
+		job_sector := jobsel.Text()
+		fmt.Println(id, title, condition, job_sector)
+
+	})
 }
 
 // source code 에서 pagination 가져와 page 개수 파악
